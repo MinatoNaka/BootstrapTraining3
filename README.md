6e73fb7a8077bf45e03c8664275d1e426f507ad5
aq_minato.naka <minato.naka@asia-quest.jp>
2019-11-21 17:35:24 +0900

    初期構成コミット

---
+++ README.md
@@ -0,0 +1,17 @@
+bootstrapの練習。  
+  
+お手本サイト  
+https://jin-demo.jp/demo-2/?p=3711
+  
+基本的にbootstrapのスタイルのみを使い、  
+お手本サイトのレイアウトを再現する。  
+
+
+# ビルド
+* パッケージインストール  
+`npm install`  
+  
+* ビルド  
+`npm run dev`  
+※Laravel Mixを使用  
+
---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.sass('src/scss/bootstrap.scss', 'dist/build/css')
+    .scripts([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ], 'dist/build/js/bundle.js');
